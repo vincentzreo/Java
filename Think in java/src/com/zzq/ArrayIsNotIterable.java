e675d8950682a2eb347c1bc9a699162a9391def7
zhouzhangqi <zhouzhangqi94@163.com>
2018-11-09 16:57:35 +0800

    容器

---
+++ Think in java/src/com/zzq/ArrayIsNotIterable.java
@@ -0,0 +1,18 @@
+package com.zzq;
+
+import java.util.Arrays;
+
+public class ArrayIsNotIterable {
+    static <T> void test(Iterable<T> ib){
+        for(T t : ib)
+            System.out.print(t+" ");
+    }
+
+    public static void main(String[] args) {
+        test(Arrays.asList(1,2,3));
+        String[] strings = {"A","B","C"};
+        //An array works in foreach , but it's not Iterable
+        //test(strings);
+        test(Arrays.asList(strings));
+    }
+}
---
+++ Think in java/src/com/zzq/QueueDemo.java
@@ -0,0 +1,26 @@
+package com.zzq;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+public class QueueDemo {
+    public static void printQ(Queue queue){
+        while (queue.peek() != null)
+            System.out.print(queue.remove() + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        Random rand = new Random(47);
+        for(int i = 0 ; i < 10 ; i ++)
+            //将元素插入队尾
+            queue.offer(rand.nextInt(i+10));
+        printQ(queue);
+        Queue<Character> qc = new LinkedList<>();
+        for(char c : "Brontosaurus".toCharArray())
+            qc.offer(c);
+        printQ(qc);
+    }
+}
